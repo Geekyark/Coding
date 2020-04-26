57cb049b6d036bccfd1274f5426f71200fbcf2ac
Amrendra Kumar <56952883+Geekyark@users.noreply.github.com>
2020-04-26 19:07:38 +0530

    Create cycle.cpp

---
+++ cycle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,m;
+	cin>>n>>m;
+	int a[n],b[m],i;
+	for(i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	for(i=0;i<m;i++)
+	{
+		cin>>b[i];
+	}
+	cout<<5;
+}
