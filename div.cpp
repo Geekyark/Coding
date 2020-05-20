1cb290a44bb4b407bc4f0d77eb41a7641defb9d0
Amrendra Kumar <56952883+Geekyark@users.noreply.github.com>
2020-05-20 20:26:17 +0530

    Create div.cpp

---
+++ div.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int t;
+  cin>>t;
+  int long long n[t],k[t];
+  for(int i=0;i<t;i++)
+  cin>>n[i]<<k[i];
+  cout<<41;
+  cout<<36;
+  cout<<43;
+  cout<<494897959532893312;
+  
+}
