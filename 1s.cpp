f47f5a99d5868c8cc524fa762d87d881f4c2be28
Amrendra Kumar <56952883+Geekyark@users.noreply.github.com>
2021-04-12 15:29:00 +0530

    Add files via upload

---
+++ 1s.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    int count=0;
+    cin>>str;
+    for(int i=0;i<str.size();i++){
+        if(str[i]==49)
+            count++;
+    }
+    cout<<count;
+}
