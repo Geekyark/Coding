b8a92cdf4f53b93d63ea3112a9975cadcf5a369e
Amrendra Kumar <56952883+Geekyark@users.noreply.github.com>
2021-04-13 11:21:42 +0530

    Add files via upload

--- 1s.cpp
+++ 1s.cpp
@@ -1,12 +1,22 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
+int indexofSmallestElement(double array[], int size);
 int main(){
-    string str;
-    int count=0;
-    cin>>str;
-    for(int i=0;i<str.size();i++){
-        if(str[i]==49)
-            count++;
-    }
-    cout<<count;
+	cout << "Enter 10 Integers" << endl;
+  for (int i = 0; i < 10; i++) {
+		cin >> numbers[i];
+  }
+	indexofSmallestElement(numbers, size);
+  return 0;
+}
+int indexofSmallestElement(double array[], int size)
+{
+	int index = 0;
+	for (int i = 1; i < size; i++)
+	{
+		if (array[i] < array[index])
+			index = i;
+	}
+	cout << endl << "Index of Smallest Element in Array is " << index << endl;
+	return index;
 }
