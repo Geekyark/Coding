6a9c368b55ab282c17ef469eae2f00ce730f80fe
Amrendra Kumar <56952883+Geekyark@users.noreply.github.com>
2021-05-19 20:54:29 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,10 @@
+console.log(3-1);
+var diff=7-3;
+console.log("diff :"+diff);
+var product=7*8;
+console.log("product :"+product);
+var div=85/17;
+console.log("div :"+div);
+var mod=56%6;
+console.log("mod :"+mod);
+console.log(1>2 ? true:false)
